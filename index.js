0f358fcf359009077695bbd473a386da0fff70b7
Rick Katka <rkatka@users.noreply.github.com>
2019-02-14 12:54:28 -0600

    Initial version of the Twilio Minnesota Wild app that will send you an SMS to notify you if the Wild won their last game.

---
+++ index.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var https = require('https');
+var app = express();
+var config = require('./config');
+const client = require('twilio')(config.accountSid, config.authToken);
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+// request to statsapi for the Minnesota Wild
+const req = https.request('https://statsapi.web.nhl.com/api/v1/teams/30?expand=team.schedule.previous', (res) => {
+  res.on('data', (d) => {
+    const data = JSON.parse(d);
+    const teamData = data.teams[0].previousGameSchedule.dates[0].games[0].teams;
+    
+    let homeTeam = teamData.home.team.id === 30 ? teamData.home : teamData.away;
+    let competitor = teamData.home.team.id === 30 ? teamData.away : teamData.home;
+    let verb;
+    let msgBody;
+
+    if( homeTeam.score > competitor.score ) {
+      // win
+      verb = 'yes';
+    } else if ( homeTeam.score === competitor.score ) {
+      // tie game
+      verb = 'tie';
+    } else {
+      // lost
+      verb = 'no';
+    }
+
+    msgBody = verb.toUpperCase() + ' ' + homeTeam.score + '-' + competitor.score + ' against the ' + competitor.team.name
+
+    sendData(msgBody);
+  });
+});
+
+req.on('error', (e)=> {
+  console.error(e.message);
+});
+
+req.end();
+
+function sendData(data) {
+  client.messages
+    .create({
+      body: 'Did the Minnesota Wild win?\n' + data,
+      from: config.from,
+      to: config.to
+    })
+    .then(message=> console.log(message.sid))
+    .done();
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Twilio Express Application to let you know if the Minnesota Wild won their last game.
+
+This is a demo app showing an example of how to use the Twilio node module with Express.
+
+To run this app, provide your Twilio credentials in a `config.js` file with the information provided in the sample config file at the project root.
